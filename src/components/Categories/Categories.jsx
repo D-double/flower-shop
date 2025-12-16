c03d5e28db150675931f87193f2cd3a18f407180
Abdulla <yakubov.abdulla05@gmail.com>
2025-12-16 23:23:27 +0500

    added swiper in Categories

--- src/components/Categories/Categories.jsx
+++ src/components/Categories/Categories.jsx
@@ -1,71 +1,136 @@
-import { images } from '../../utils'
-import s from './categories.module.css'
-import CategoriesItem from './CategoriesItem'
+import { images } from "../../utils";
+import CategoriesItem from "./CategoriesItem";
+import { Swiper, SwiperSlide } from "swiper/react";
+import s from "./categories.module.css";
+import "swiper/css";
+import { Navigation } from "swiper/modules";
+import "swiper/css/navigation";
 
 const Categories = () => {
-    const catList = [
+  const catList = [
+    {
+      title: "Цветы",
+      products: [
         {
-            title: 'Цветы',
-            products: [
-                {
-                    name: 'Классические букеты',
-                    image: images.flower1
-                },
-                {
-                    name: 'Авторские букеты',
-                    image: images.flower2
-                },
-                {
-                    name: 'Монобукеты',
-                    image: images.flower3
-                },
-            ]
+          name: "Классические букеты",
+          image: images.flower1,
         },
         {
-            title: 'Торты',
-            products: [
-                {
-                    name: 'Бенто',
-                    image: images.cake1
-                },
-                {
-                    name: 'Сникерс',
-                    image: images.cake2
-                },
-                {
-                    name: 'Тирамису',
-                    image: images.cake3
-                },
-            ]
+          name: "Авторские букеты",
+          image: images.flower2,
         },
         {
-            title: 'Воздушные шары',
-            products: [
-                {
-                    name: 'Шары в коробке',
-                    image: images.flower1
-                },
-                {
-                    name: 'Наборы шаров',
-                    image: images.flower2
-                },
-                {
-                    name: 'Шары с надписями',
-                    image: images.flower3
-                },
-            ]
+          name: "Монобукеты",
+          image: images.flower3,
         },
+      ],
+    },
+    {
+      title: "Торты",
+      products: [
+        {
+          name: "Бенто",
+          image: images.cake1,
+        },
+        {
+          name: "Сникерс",
+          image: images.cake2,
+        },
+        {
+          name: "Тирамису",
+          image: images.cake3,
+        },
+      ],
+    },
+    {
+      title: "Воздушные шары",
+      products: [
+        {
+          name: "Шары в коробке",
+          image: images.flower1,
+        },
+        {
+          name: "Наборы шаров",
+          image: images.flower2,
+        },
+        {
+          name: "Шары с надписями",
+          image: images.flower3,
+        },
+      ],
+    },
+    {
+      title: "Цветы",
+      products: [
+        {
+          name: "Классические букеты",
+          image: images.flower1,
+        },
+        {
+          name: "Авторские букеты",
+          image: images.flower2,
+        },
+        {
+          name: "Монобукеты",
+          image: images.flower3,
+        },
+      ],
+    },
+    {
+      title: "Торты",
+      products: [
+        {
+          name: "Бенто",
+          image: images.cake1,
+        },
+        {
+          name: "Сникерс",
+          image: images.cake2,
+        },
+        {
+          name: "Тирамису",
+          image: images.cake3,
+        },
+      ],
+    },
+    {
+      title: "Воздушные шары",
+      products: [
+        {
+          name: "Шары в коробке",
+          image: images.flower1,
+        },
+        {
+          name: "Наборы шаров",
+          image: images.flower2,
+        },
+        {
+          name: "Шары с надписями",
+          image: images.flower3,
+        },
+      ],
+    },
+  ];
 
-    ]
+  
   return (
     <div className={s.categories}>
-        {
-            catList.map((elem, index)=>(
-                <CategoriesItem key={index} {...elem}/>
-            ))
-        }
+      <Swiper
+        spaceBetween={50}
+        slidesPerView={3}
+        onSlideChange={() => console.log("slide change")} 
+        navigation={true}
+        modules={[Navigation]}
+      >
+        {catList.map((elem, index) => (
+          <SwiperSlide>
+            <CategoriesItem key={index} {...elem} />
+          </SwiperSlide>
+        ))}
+      </Swiper>
     </div>
-  )
-}
 
-export default Categories
+  );
+};
+
+export default Categories;
